3614c538e9c4a1a689b145a26a7bba97fc3493f8
Epic Berry <epicbrry@gmail.com>
2016-11-12 23:07:40 -0500

    fetching data from service deployed in nodejs

---
+++ sampleTestData.js
@@ -0,0 +1,42 @@
+// 	$scope.testCases = [
+// 		{
+//       "testCaseId" : "1",
+// 			"testCategory": "cat1",
+// 			"includeInExecution" : "true",
+// 			"description" : "",
+// 			"basic" : {
+// 				"browser" : "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0",
+// 	      "url" : "http://www.google.com",
+// 			},
+//       "steps" : {
+// 				"testQuery" : "github",
+//       }
+//     },
+// 		{
+//       "testCaseId" : "2",
+// 			"testCategory": "cat1",
+// 			"includeInExecution" : "true",
+// 			"description" : "",
+// 			"basic" : {
+// 				"browser" : "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0",
+// 	      "url" : "http://www.google.com",
+// 			},
+//       "steps" : {
+// 				"testQuery" : "github",
+//       }
+//     },
+// 		{
+//       "testCaseId" : "3",
+// 			"testCategory": "cat2",
+// 			"includeInExecution" : "false",
+// 			"description" : "",
+// 			"basic" : {
+// 				"browser" : "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0",
+// 	      "url" : "http://www.google.com",
+// 			},
+//       "steps" : {
+// 				"testQuery" : "github",
+//       }
+//     },
+// 	]
+// });

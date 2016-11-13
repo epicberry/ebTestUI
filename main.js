c43e6943ab49864eda7a36435f2bb23ea570ad2e
Epic Berry <epicbrry@gmail.com>
2016-11-13 00:10:37 -0500

    fixed bidirectional binding

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ app.controller('TestCaseController', function($scope, $http) {
     $scope.selectedIndex = null;
     $scope.selectedTestCase = null;
 
+    $http({
+        method: 'GET',
+				url: 'http://192.169.179.82:1111/data/all'
+        //url: 'http://localhost:1111/data/all'
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        $scope.testCases = response.data;
+
+
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        $scope.testCases = [{}]
+    });
+
     $scope.selectTestCase = function(testCase, index) {
         $scope.selectedIndex = index;
         $scope.selectedTestCase = testCase;
@@ -21,27 +37,12 @@ app.controller('TestCaseController', function($scope, $http) {
     };
 
     $scope.runTestCases = function(testCases) {
-        console.log(testCases);
+        //console.log(testCases);
         // var testCasesToBeRun = $scope.testCases.filter(function(t){return t.includeInExecution == 'true';});
         var testCasesToBeRun = testCases.filter(function(t) {
-            return t.includeInExecution == 'true';
+            return t.includeInExecution == true;
         });
         //console.log(testCasesToBeRun);
         console.log(JSON.stringify(testCasesToBeRun));
     };
-
-    // Simple GET request example:
-    $http({
-        method: 'GET',
-				url: 'http://192.169.179.82:1111/data/all'
-        // url: 'http://localhost:8082/data/all'
-    }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-        $scope.testCases = response.data;
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        $scope.testCases = [{}]
-    });
 });
